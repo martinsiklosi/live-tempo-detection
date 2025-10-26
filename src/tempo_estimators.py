43d17e2f10059bc2b68e89391d3cfe357f0d40bf
Martin Siklosi <martin@siklosi.se>
2025-10-26 19:04:33 +0100

    Fixed audio trimming

--- src/tempo_estimators.py
+++ src/tempo_estimators.py
@@ -7,6 +7,9 @@ from src.novelty import phase_novelty
 
 
 class PhaseNoveltyEstimator(AbstractTempoEstimator):
+    TRIM_THRESHOLD_FACTOR = 2
+    TRIM_KEEP_FACTOR = 1.1
+
     def __init__(
         self,
         initial_tempo: float,
@@ -27,39 +30,32 @@ class PhaseNoveltyEstimator(AbstractTempoEstimator):
             self.audio.s_to_samples(stft_window_size_s),
             self.audio.s_to_samples(stft_hop_size_s),
         )
-        self.estimation_window: float = estimation_window
+        self.estimation_window_s: float = estimation_window
 
         self.n_forgotten_audio_samples: int = 0
 
     def listen(self, new_audio: Audio) -> float | None:
         self.audio.append(new_audio)
+        self._trim_audio()
         self.stft.update(self.audio.samples, offset=self.n_forgotten_audio_samples)
 
-        # Forget irrelevant audio samples - i.e. all samples that are before the next window
-        current_n_samples = len(self.audio)
-        n_to_forget = self.stft.win_length - current_n_samples
-        if n_to_forget > 0:
-            # self.audio.samples = self.audio.samples[n_to_forget:]
-            self.audio.delete_from_start(n_to_forget)
-            self.n_forgotten_audio_samples += n_to_forget
-
-        relevant_frame_count = int(self.estimation_window / self.stft_hop_size_s) + 1
+        relevant_frame_count = int(self.estimation_window_s / self.stft_hop_size_s) + 1
         relevant_frames = self.stft.last_n_frames(relevant_frame_count)
         # Compute flux from relevant_frames
 
         if relevant_frames.shape[1] < relevant_frame_count:
             return None
 
-        pn = phase_novelty(relevant_frames)
+        raw_pn = phase_novelty(relevant_frames)
 
         moving_window_size = min(int(0.2 / self.stft_hop_size_s), 3)  # 0.2 seconds
-        pn -= np.convolve(
-            pn,
+        pn = raw_pn - np.convolve(
+            raw_pn,
             np.ones(moving_window_size) / moving_window_size,
             mode="same",
         )
-
         pn[pn < 0] = 0
+
         tempo, has_beat = tempo_distribution_around_guess(
             pn, frame_duration=self.stft_hop_size_s, initial_guess=self.current_tempo
         )
@@ -68,6 +64,17 @@ class PhaseNoveltyEstimator(AbstractTempoEstimator):
             self.current_tempo = tempo
             return self.current_tempo
         return None
+
+    def _trim_audio(self) -> None:
+        """
+        Trim audio in case it gets too long.
+        """
+        if self.audio.duration > self.estimation_window_s * self.TRIM_THRESHOLD_FACTOR:
+            n_to_forget = len(self.audio) - self.audio.s_to_samples(
+                self.estimation_window_s * self.TRIM_KEEP_FACTOR
+            )
+            self.audio.delete_from_start(n_to_forget)
+            self.n_forgotten_audio_samples += n_to_forget
 
 
 def LongWindow(initial_tempo: float, sr: int) -> PhaseNoveltyEstimator:
